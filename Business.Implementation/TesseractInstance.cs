9f6fed327b27ff9a79e5a7a3eb612b42310742b0
Vadzim Adamtsevich <vadamtsevich@thinktime.com>
2019-11-28 14:07:50 +0300

    improve ocr

--- Business.Implementation/TesseractInstance.cs
+++ Business.Implementation/TesseractInstance.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +12,20 @@ using Tesseract;
 
 namespace Business.Implementation
 {
-    public class TesseractInstance: IOcrEngine
+    public class TesseractInstance: IOcrEngine, IDisposable
     {
-        private static Dictionary<SupportedLangs,string> _supportedLangDictionary=new Dictionary<SupportedLangs, string>()
-        {
-            {SupportedLangs.Ru, "rus"},
-            {SupportedLangs.En, "eng"},
-        };
-        private static string path = String.Empty;
+        private readonly Dictionary<SupportedLangs, string> _supportedLangDictionary =
+            new Dictionary<SupportedLangs, string>()
+            {
+                {SupportedLangs.Ru, "rus"},
+                {SupportedLangs.En, "eng"},
+            };
 
-        static TesseractInstance()
+        private readonly ConcurrentDictionary<string, TesseractEngine> _supportedTSEDictionary = new ConcurrentDictionary<string, TesseractEngine>();
+
+        private readonly string path = String.Empty;
+
+        public TesseractInstance()
         {
             path = AppDomain.CurrentDomain.BaseDirectory + "Tesseract";
         }
@@ -28,7 +34,7 @@ namespace Business.Implementation
         public string ReadText(string base64, SupportedLangs lang)
         {
             byte[] imageBytes = Convert.FromBase64String(base64);
-            using (var stream = new System.IO.MemoryStream(imageBytes))
+            using (MemoryStream stream = new System.IO.MemoryStream(imageBytes))
             using (Bitmap bitImage = new Bitmap(Image.FromStream(stream)))
             {
                 return this.ReadText(bitImage, lang);
@@ -43,23 +49,35 @@ namespace Business.Implementation
 
             if (_supportedLangDictionary.TryGetValue(lang, out langValue))
             {
-                using (var engine = new TesseractEngine(path, langValue, EngineMode.Default))
+                var engine = _supportedTSEDictionary.GetOrAdd(langValue,
+                    s => { return new TesseractEngine(path, langValue, EngineMode.Default); });
+
+                using (var img = PixConverter.ToPix(imgsource))
                 {
-                    using (var img = PixConverter.ToPix(imgsource))
+                    using (var page = engine.Process(img))
                     {
-                        using (var page = engine.Process(img))
-                        {
-                            ocrtext = page.GetText();
-                        }
+                        ocrtext = page.GetText();
                     }
                 }
+
             }
             else
             {
                 throw new ArgumentOutOfRangeException("unsupported lang");
             }
+
             return ocrtext;
         }
-    
+
+        public void Dispose()
+        {
+            foreach (var tesseractEngine in _supportedTSEDictionary)
+            {
+                if (!tesseractEngine.Value.IsDisposed)
+                {
+                    tesseractEngine.Value.Dispose();
+                }
+            }
+        }
     }
 }
